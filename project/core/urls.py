b2b041245ec1b5a5117b669c99c5a2259f6f4f8b
Carlos Augusto Moreno <omorenodovale@gmail.com>
2019-10-28 12:04:00 -0500

    Optimize imports

--- project/core/urls.py
+++ project/core/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import Launches, NextLaunch, UpcomingLaunch, LatestLaunch, PastLaunch
+from .views import NextLaunch, UpcomingLaunch, LatestLaunch, PastLaunch
 
 urlpatterns = [
     path("next", NextLaunch.as_view(), name="next"),
